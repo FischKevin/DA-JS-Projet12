d45f6b0d3b3be6970c90d4f237036a2dbddb5de9
Kévin Fischer <kevinfischer@live.fr>
2024-01-22 10:39:48 +0100

    :adhesive_bandage: fixed img import on login page for Verccel deployment

--- src/components/login/UserCard.jsx
+++ src/components/login/UserCard.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import KarlImage from '/src/assets/users/karl.jpg';
+import CeciliaImage from '/src/assets/users/cecilia.jpg';
+
 function UserCard({ user }) {
+  let userImage;
+  if (user.firstName === 'Karl') {
+    userImage = KarlImage;
+  } else if (user.firstName === 'Cecilia') {
+    userImage = CeciliaImage;
+  }
+
   return (
     <div className="user-card">
       <Link to={`/dashboard/${user.id}`}>
-        <img src={user.photo} alt={user.firstName} />
+        <img src={userImage} alt={user.firstName} />
         <p>{user.firstName}</p>
       </Link>
     </div>
